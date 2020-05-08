5005930a3eef343905eb266368e8ef0a55ff6f2e
Joao Cordeiro <joao.cordeiro@isoftbet.com>
2020-05-08 23:52:13 +0100

    add preliminary method to parse movies

--- parse_performance_test.py
+++ parse_performance_test.py
@@ -38,6 +38,39 @@ def parse_strfind(page_contents):
 
     return (following, page_next)
 
+def extract_movies(page_contents):
+    movies = []
+
+    def extract_value(str, key, start):
+        start = str.find(key, start) + len(key)
+        end = str.find("\"", start)
+        return (start, end, str[start : end])
+
+    start = 1
+    while start > 0:
+        start = page_contents.find("poster-container", start)
+        if start > 0:
+            (start, _, movie_name) = extract_value(page_contents, "data-target-link=\"/film/", start)
+            (_, start, movie_rate) = extract_value(page_contents, " rated-", start)
+
+            movies.append((movie_name[:-1], int(movie_rate)))
+
+
+    # pagination
+
+    key_page_next = "\"next\" href=\""
+
+    page_next = None
+    start = page_contents.rfind("paginate-nextprev")
+    start = page_contents.find(key_page_next, start)
+    if start > 0:
+        start += len(key_page_next)
+        end_idx = page_contents.find("\"", start)
+        page_next = page_contents[start + 1 : end_idx]
+
+    return (movies, page_next)
+
+
 def validate(expected, result):
     if (result != expected):
         print("Error!\nExpected: {}\nGot: {}".format(expected, result))
@@ -45,12 +78,18 @@ def validate(expected, result):
         print("ok")
 
 if __name__ == "__main__":
-    def run_tests(test_name, test_func, test_data, n_cycles, runs_per_cycle):
+    def run_tests(test_name, test_func, test_data_filename):
+        with open(test_data_filename, 'r') as file:
+            test_data = file.read()
+
+        n_cycles = 5
+        n_runs   = 100
+
         print("== ", test_name)
         absolute_start_time = datetime.now()
         for x in range(n_cycles):
             start_time = datetime.now()
-            for _ in range(runs_per_cycle):
+            for _ in range(n_runs):
                 output = test_func(test_data)
 
             end_time = datetime.now()
@@ -60,16 +99,18 @@ if __name__ == "__main__":
         return output
 
 
-    n_cycles = 5
-    n_runs   = 100
-
     def compare_tests(filename):
-        with open(filename, 'r') as file:
-            data = file.read()
-
-        result = run_tests("string scanning", parse_strfind, data, n_cycles, n_runs)
-        expected = run_tests("BeautifulSoup", parse_beautiful_soup, data, n_cycles, n_runs)
+        result = run_tests("string scanning", parse_strfind, filename)
+        expected = run_tests("BeautifulSoup", parse_beautiful_soup, filename)
         validate(expected, result)
 
     compare_tests("testdata/jlcordeiro_following_1.html")
     compare_tests("testdata/jlcordeiro_following_2.html")
+
+    print("----- running preliminary tests for movie pages parsing  -------")
+
+    m1 = run_tests("movies1", extract_movies, "testdata/jlcordeiro_watched_1.html")
+    m3 = run_tests("movies3", extract_movies, "testdata/jlcordeiro_watched_3.html")
+    m7 = run_tests("movies7", extract_movies, "testdata/jlcordeiro_watched_7.html")
+
+    print(m1[1], m3[1], m7[1])
