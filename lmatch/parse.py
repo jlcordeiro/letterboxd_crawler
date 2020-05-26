02f744b0a9d0683456a02d82b6d7e4b96e96a84e
Joao Cordeiro <jlcordeiro@gmail.com>
2020-05-26 02:20:52 +0100

    add documentation and type hints to parse.py

--- lmatch/parse.py
+++ lmatch/parse.py
@@ -1,28 +1,54 @@
+from typing import List, Tuple, Union
 
-def _extract_value(str, key, start):
-    start = str.find(key, start)
+def _extract_value(data, key: str, start: int) -> Tuple[int, int, str]:
+    """ Find and extract <value> out of a
+        ..key": "<value>"....
+
+        Returns a tuple with
+        - the index to the start of the value
+        - the index to the end of the value
+        - the string with the value found
+
+        If not found (-1, -1, "")
+    """
+    start = data.find(key, start)
     if start is -1:
         return (-1, -1, "")
 
     start += len(key)
-    end = str.find("\"", start)
-    return (start, end, str[start:end])
+    end = data.find("\"", start)
+    return (start, end, data[start:end])
 
 
-def next_page(str):
+def next_page(page: str) -> Union[None, str]:
+    """ Given the contents of a Letterboxd page, returns
+        the relative path to the next page to parse. It
+        handles the pagination of any type of page,
+        from followers, to following, to movies watched,
+        etc.
+
+        Returns None if this is the last page already
+        and there isn't another one to parse.
+    """
     key_page_next = "\"next\" href=\""
 
-    start = str.rfind("paginate-nextprev")
-    start = str.find(key_page_next, start)
+    start = page.rfind("paginate-nextprev")
+    start = page.find(key_page_next, start)
     if start is -1:
         return None
 
     start += len(key_page_next)
-    end_idx = str.find("\"", start)
-    return str[start + 1:end_idx]
+    end_idx = page.find("\"", start)
+    return page[start + 1:end_idx]
 
 
-def following(page):
+def following(page: str) -> List[str]:
+    """ Given a Letterboxd 'following' page, parses out the
+    list of usernames followed in it.
+
+    Warning: Not a lot of checks are done, if the wrong page
+    is passed, most likely it'll just return an empty list.
+    """
     following = []
 
     start = 1
@@ -35,7 +61,23 @@ def following(page):
     return following
 
 
-def movies_watched(page):
+def movies_watched(page: str) -> List[Tuple[str, int]]:
+    """ Given a Letterboxd 'films' page, parses out the
+    list of movies watched by the user, as well as the
+    rating given to each.
+
+    The rating is in a scale [0, 10], wit the default value
+    being 0, in case the user didn't rate the movie at all.
+
+    Returns a list of tuples:
+        [
+            ('movie-a', 7),
+            ('movie-b', 2)
+        ]
+
+    Warning: Not a lot of checks are done, if the wrong page
+    is passed, most likely it'll just return an empty list.
+    """
     movies = []
 
     tag_movie_container = "poster-container"
